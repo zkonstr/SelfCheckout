16713bcc07d0dbbd54c89857e33be78c46f1058a
Konstantin <zkonstr@gmail.com>
2021-07-14 17:31:09 +0300

    Refactor : namespace "Cart" has been renamed to "Shop"

--- Shop/Cart.cs
+++ Shop/Cart.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace SelfCheckuot.Cart
+namespace SelfCheckuot.Shop
 {
     public class Cart
     {
--- Shop/Product.cs
+++ Shop/Product.cs
@@ -1,6 +1,4 @@
-﻿using System.Management.Instrumentation;
-
-namespace SelfCheckuot.Cart
+﻿namespace SelfCheckuot.Shop
 {
     public class Product
     {
