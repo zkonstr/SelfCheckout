c34e2130c1df4fc27a51e4facde72402a30cb613
Konstantin <zkonstr@gmail.com>
2021-07-14 17:26:27 +0300

    Cart and products for it have been added

---
+++ Cart/Cart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SelfCheckuot.Cart
+{
+    public class Cart
+    {
+        private decimal _sum=0;
+        private List<Product> _products;
+
+        public List<Product> Products
+        {
+            get => _products.ToList();
+            private set => _products = Products;
+        }
+
+        public Cart()
+        {
+            Products = new List<Product>();
+        }   
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+            _sum += product.Cost;
+        }
+
+        public void Delete(int indexOfProduct)
+        {
+            _sum -= Products[indexOfProduct].Cost;
+            Products.RemoveAt(indexOfProduct);
+        }
+    }
+}
---
+++ Cart/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Management.Instrumentation;
+
+namespace SelfCheckuot.Cart
+{
+    public class Product
+    {
+        public decimal Cost { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Product(int id, string name, decimal cost)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+        }
+    }
+}
