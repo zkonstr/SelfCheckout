b81b63612f157e1e7140c6e9ba2d60e2f8415a06
Konstantin <zkonstr@gmail.com>
2021-08-07 14:31:37 +0300

    Feat : SQLite DB added

---
+++ Database/StoreSqliteDao.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using SelfCheckuot.Shop;
+
+namespace SelfCheckuot.Database
+{
+    public class StoreSqliteDao : IStoreDao
+    {
+        private SQLiteConnection _connection;
+        private string _tableName;
+
+        public StoreSqliteDao(string tableName)
+        {
+            _tableName = tableName;
+        }
+
+        public bool Connect(string url)
+        {
+            var connString = $"Data Source={url};Version=3;";
+            _connection = new SQLiteConnection(connString);
+            using (_connection)
+            {
+                _connection.Open();
+            }
+
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText = $"CREATE TABLE {_tableName} IF NOT EXISTS";
+            return true;
+        }
+
+        public bool Disconnect()
+        {
+            using (_connection)
+            {
+                _connection.Close();
+            }
+
+            return true;
+        }
+
+        public bool InsertProduct(Product product)
+        {
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText =
+                $"INSERT INTO {_tableName} (ID,NAME,COST) VALUES({product.Id},'{product.Name}',{product.Cost});";
+            return cmd.ExecuteScalar() != null;
+        }
+
+        public Product ReadProduct(int id)
+        {
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM {_tableName} WHERE ID = {id};";
+            var reader = cmd.ExecuteReader();
+            Product product = new Product();
+            while (reader.Read())
+            {
+                product.Id = (int) reader["ID"];
+                product.Name = (string) reader["NAME"];
+                product.Cost = (decimal) reader["COST"];
+            }
+
+            return product;
+        }
+
+        public Product[] ReadAllProducts()
+        {
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM {_tableName};";
+            var reader = cmd.ExecuteReader();
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                var product = new Product();
+                product.Id = (int) reader["ID"];
+                product.Name = (string) reader["NAME"];
+                product.Cost = (decimal) reader["COST"];
+                products.Add(product);
+            }
+            return products.ToArray();
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText =
+                $"UPDATE {_tableName} SET NAME = {product.Name}, COST = {product.Cost} WHERE ID= {product.Id};";
+            return cmd.ExecuteScalar() != null;
+        }
+
+        public bool DeleteProduct(int id)
+        {
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText = $"DELETE * FROM {_tableName} WHERE ID = {id};";
+            return cmd.ExecuteScalar() != null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using SelfCheckuot.Shop;
 
@@ -8,54 +7,56 @@ namespace SelfCheckuot
 {
     internal class Program
     {
-        private static Dictionary<int, Product> store = new Dictionary<int, Product>();
-        private static Cart cart = new Cart();
+        private static Dictionary<int, Product> _store = new Dictionary<int, Product>();
+        private static Cart _cart = new Cart();
 
-        public static void Help()
+        private static void Help()
         {
-            foreach (var item in store)
+            foreach (var item in _store)
             {
                 Console.Out.WriteLine("Id of " + item.Value.Name + " is " + item.Value.Id);
             }
         }
 
-        public static void AddProduct(string strIndex)
+        private static void AddProduct(string strIndex)
         {
             int id;
             try
             {
                 id = int.Parse(strIndex);
-                cart.Add(store[id]);
+                _cart.Add(_store[id]);
             }
             catch (Exception e)
             {
                 Console.WriteLine("incorrect id, please try again");
+                Console.WriteLine("error: " + e);
                 return;
             }
 
-            Console.WriteLine(store[id].Name + " added to cart " + " Cost = {0}",
-                store[id].Cost + " Sum = " + cart.Sum);
+            Console.WriteLine(_store[id].Name + " added to cart " + " Cost = {0}",
+                _store[id].Cost + " Sum = " + _cart.Sum);
         }
 
-        public static void DeleteProduct()
+        private static void DeleteProduct()
         {
             Console.Out.WriteLine("Delete products by entering its position");
             string index = Console.ReadLine();
             try
             {
-                int position = int.Parse(index);
-                cart.Delete(position - 1);
+                int position = int.Parse(index ?? string.Empty);
+                _cart.Delete(position - 1);
             }
             catch (Exception e)
             {
                 Console.WriteLine("wrong position");
+                Console.WriteLine("error: " + e);
                 return;
             }
 
             Console.Out.WriteLine("Delete successfully");
         }
 
-        public static void CLI()
+        private static void Cli()
         {
             Console.Out.WriteLine("Add any products to UR cart ");
             Console.Out.WriteLine("Enter 'help' to see all Ids ");
@@ -76,7 +77,7 @@ namespace SelfCheckuot
                 }
             }
 
-            Console.WriteLine("Proceed to checkout " + "Sum = {0}", cart.Sum);
+            Console.WriteLine("Proceed to checkout " + "Sum = {0}", _cart.Sum);
             PrintCheck();
         }
 
@@ -84,24 +85,25 @@ namespace SelfCheckuot
 
         private const string OutputFolder = @".\checks\";
         private const string OutputExtension = ".txt";
-        public static void PrintCheck()
+
+        private static void PrintCheck()
         {
             var fileName = DateTime.Now.ToString().Replace(':', '-');
             using (StreamWriter writer = new StreamWriter(OutputFolder + fileName + OutputExtension))
             {
-                foreach (var product in cart.Products)
+                foreach (var product in _cart.Products)
                 {
                     writer.WriteLine(product.Name+" - "+product.Cost);
                 }
-                writer.WriteLine("Sum = {0}", cart.Sum);
+                writer.WriteLine("Sum = {0}", _cart.Sum);
             }
         }
 
         public static void Main(string[] args)
         {
             
-            store=StoreXmlSerializer.Load();
-            CLI();
+            _store=StoreXmlSerializer.Load();
+            Cli();
             
         }
     }
