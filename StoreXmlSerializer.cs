7a760c16e62f9abed54c9c23ce789984d44a1677
Konstantin <zkonstr@gmail.com>
2021-08-07 18:37:43 +0300

    Feat : database is used by application

--- StoreXmlSerializer.cs
+++ StoreXmlSerializer.cs
@@ -11,17 +11,14 @@ namespace SelfCheckuot
         public static Dictionary<int, Product> Load()
         {
             List<Product> products;
-            using (StreamReader fs = new StreamReader("check.xml"))
+            using (var fs = new StreamReader("check.xml"))
             {
-                XmlSerializer xs = new XmlSerializer(typeof(List<Product>));
+                var xs = new XmlSerializer(typeof(List<Product>));
                 products = (List<Product>) xs.Deserialize(fs);
             }
 
             var store = new Dictionary<int, Product>();
-            foreach (var product in products)
-            {
-                store[product.Id] = product;
-            }
+            foreach (var product in products) store[product.Id] = product;
 
             return store;
         }
@@ -29,9 +26,9 @@ namespace SelfCheckuot
         public static void Save(Dictionary<int, Product> store)
         {
             var products = store.Values.ToList();
-            using (StreamWriter fs = new StreamWriter("check.xml"))
+            using (var fs = new StreamWriter("check.xml"))
             {
-                XmlSerializer xs = new XmlSerializer(typeof(List<Product>));
+                var xs = new XmlSerializer(typeof(List<Product>));
                 xs.Serialize(fs, products);
             }
         }
